35c12126a529783bbd1fc1e8b8dff69e6295b8c1
jeffersondazacaro <60698493+jeffersondazacaro@users.noreply.github.com>
2020-04-22 08:38:20 -0500

    Add files via upload

---
+++ Ejercicios_maraton/Promedio.py
@@ -0,0 +1,12 @@
+print("Ingrese la primera nota")
+
+n1=input()
+n1=float(n1)
+print("Ingrese la segunda nota")
+
+n2=input()
+n2=float(n2)
+
+resultado=(n1+n2)/2
+
+print("El resultado es: ", resultado)
